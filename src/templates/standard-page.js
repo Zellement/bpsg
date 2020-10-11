8c618fe24ffb55278b914d222a40caf5a625b3d3
Dan Farrow <dan.farrow@adtrak.co.uk>
2020-10-11 16:02:45 +0100

    Minor stuffs

--- src/templates/standard-page.js
+++ src/templates/standard-page.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql } from 'gatsby'
 import SEO from "../components/seo"
 import { motion } from "framer-motion"
 import { HTMLContent } from "../components/content"
@@ -33,23 +34,26 @@ const StandardPage = ({ data }) => {
         variants={container}
         initial="hidden"
         animate="visible"
-        className="container"
+        className=""
       >
-        <motion.div className="p-4" variants={item} transition="easeInOut">
+        <motion.div className="container p-4" variants={item} transition="easeInOut">
           
-          <p className="m-0 text-gray-500 lowercase font-slab">Birmingham Parents Support Sroup</p>
-          <HTMLContent className="text-2xl font-bold leading-tight text-black lowercase font-slab hero-primary" content={post.frontmatter.title} />
-          
-          <Img className="mt-8" fluid={post.frontmatter.image.childImageSharp.fluid} />
+          <p className="m-0 text-gray-500 lowercase font-slab">Birmingham Parents Support Group</p>
+          <HTMLContent className="text-2xl font-bold leading-tight text-black lowercase font-slab hero-primary lg:text-3xl xl:text-4xl" content={post.frontmatter.title} />
 
         </motion.div>
 
         <motion.div
-          className="p-8 content"
+          className="items-stretch p-4 lg:flex lg:flex-row"
           variants={item}
           transition="easeInOut"
         >
-          <HTMLContent content={post.html} />
+          
+          <div className="flex flex-col w-full h-40 mb-8 lg:h-auto sm:h-56 md:h-64 lg:w-7/12 lg:mb-0">
+            <Img className="w-full h-full object-fit" fluid={post.frontmatter.image.childImageSharp.fluid} />
+          </div>
+
+          <HTMLContent className="z-10 bg-white lg:p-16 lg:shadow-lg content lg:w-7/12 lg:-ml-8 lg:mt-16 lg:mb-16 lg:h-auto" content={post.html} />
         </motion.div>
       </motion.section>
     </>
@@ -66,7 +70,7 @@ export const StandardPageQuery = graphql`
         title
         image {
           childImageSharp {
-            fluid(maxWidth: 1000, maxHeight: 330) {
+            fluid(quality: 90, maxHeight: 1200) {
               ...GatsbyImageSharpFluid
             }
           }
